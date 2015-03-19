85aa686f4466dc001bc2916d148ddeccb5b82db4
Bodiok007 <bodiahrubach@mail.ru>
2015-03-19 14:29:19 +0200

    visit

---
+++ client/views/skills/skill.js
@@ -0,0 +1,10 @@
+Template.skill.helpers({
+  sk: function() {
+    return [
+      "Мови програмування: С++, C#, JavaScript",
+      "Фреймворки:  QT, .NET, Meteor",
+      "Операційні системи:  Windows, Linux",
+      "Іноземні мови:  англійська"
+    ];
+  }
+});
